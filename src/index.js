42c4ff8c40a27cecaf4a76dc9a9bd916a1fc1597
Christian <christiansamuelpaul@gmail.com>
2017-08-20 12:43:48 -0400

    neaten dir structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import AppContainer from './containers/AppContainer.js';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
