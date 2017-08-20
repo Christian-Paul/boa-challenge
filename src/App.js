c159a759f2842fd4acb7e2767895426b03a8f5f8
Christian <christiansamuelpaul@gmail.com>
2017-08-20 00:40:32 -0400

    add AppContainer

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       <div className='app'>
         <Navbar page='Sports Magazine' />
         <Settings />
-        <Modal />
+        {this.props.activeModal && <Modal />}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AppContainer from './containers/AppContainer.js';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux'
@@ -11,7 +11,7 @@ let store = createStore(reducer)
 
 ReactDOM.render(
 	<Provider store={store}>
-		<App />
+		<AppContainer />
 	</Provider>, 
 	document.getElementById('root')
 );
